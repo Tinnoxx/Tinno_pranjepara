b166d74b4fd72edb81a12ab2fb8e18b45d17b31e
Tinno <96495011+Tinnoxx@users.noreply.github.com>
2021-12-30 23:33:13 +0100

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,13 @@
+description "Pranje novca za vaš server"
+
+author "Tinno#1596"
+
+server_scripts {
+    'config.lua',
+    'server/main.lua'
+}
+
+client_scripts {
+    'config.lua',
+    'client/main.lua'
+}
---
+++ config.lua
@@ -0,0 +1,7 @@
+Config = {}
+
+Config.Location = vector3(OVDJE IDE VAŠA LOKACIJAA)
+Config.MaxDistance = 4
+Config.ClickDistance = 2
+
+}
